467986bdf817257dc0da237dffa145dcfb8587ae
nazmulsakib658-cloud <nazmulsakib658@gmail.com>
2026-05-30 10:03:15 +0600

    index.js

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ app.get('/', (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log('Server is running'));
